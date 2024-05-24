ec9a489cf143d8165db0da8458bbf4649a516c90
@farissahman1 <farissahman1@gmail.com>
2024-05-24 14:23:54 +0200

    reorganised files for deploy on render

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ const redisClient = require("./utils/redisClient.js");
 const authRoutes = require("./routes/authRoutes.js");
 const chatController = require("./controllers/chatController.js");
 const authMiddleware = require("./middlewares/authMiddleware.js");
+const path = require("path");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 const app = express();
 const server = http.createServer(app);
@@ -32,6 +34,12 @@ app.use(
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
